523ea415e6da54d897382d9de095e1dcadd5eef5
Hu <huang.h0709@gmail.com>
2023-09-19 19:14:17 -0400

    updated leftArrow component css format

--- src/components/LeftArrow/LeftArrow.jsx
+++ src/components/LeftArrow/LeftArrow.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
-import arrows from '../models/arrows'
+import arrows from '../../models/arrows'
 import { Link } from 'react-router-dom'
+import styles from './LeftArrow.module.css'
 
 function LeftArrow() {
     return (
         <div>
             <Link to="/" style={{ textDecoration: 'none', color: '#C3CEDA' }}>
-                <div className="leftArrow">{arrows.left}</div>
+                <div className={`${styles.leftArrow}`}>{arrows.left}</div>
             </Link>
         </div>
     )
